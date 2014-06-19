c8d27bb2db79c118266cf1b3aa7139ed5750d3a0
Antonio Zanardo <zanardo@gmail.com>
2014-06-19 16:09:12 -0300

    do not check ssh id

--- winbox.sh
+++ winbox.sh
@@ -8,6 +8,7 @@ PORT=$(docker port winbox 22 | sed -e 's/^.*://')
 
 for x in $(seq 10); do
 	sshpass -pwinbox ssh -X -p $PORT -o StrictHostKeyChecking=no \
+		-o UserKnownHostsFile=/dev/null \
 		winbox@127.0.0.1 wine /winbox.exe && break
 	sleep 1
 done
