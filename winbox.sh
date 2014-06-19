a5142cd2f0bd913d38f0a12c2ca37f9594fa9a6d
Antonio Zanardo <zanardo@gmail.com>
2014-06-19 15:51:40 -0300

    add Dockerfile and winbox.sh

---
+++ winbox.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+docker start winbox || docker run --name winbox -d -p 127.0.0.1::22 zanardo/winbox
+
+PORT=$(docker port winbox 22 | sed -e 's/^.*://')
+
+for x in $(seq 10); do
+	sshpass -pwinbox ssh -X -p $PORT -o StrictHostKeyChecking=no \
+		winbox@127.0.0.1 wine /winbox.exe && break
+	sleep 1
+done
+
+docker stop winbox
